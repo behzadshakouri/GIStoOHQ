40fdb9beb864d8ae0c0c24b497fefaafc8071060
Behzad Shakouri <behzad.shakouri92@gmail.com>
2026-06-26 18:05:22 -0400

    Add files via upload

---
+++ scripts/writer/write_ohq.py
@@ -0,0 +1,11 @@
+"""Generate an OpenHydroQual (.ohq) model from GIS outputs.
+
+Inputs:
+- topology.gpkg
+- reaches.gpkg
+- subwatershed_params.gpkg
+- junctions.gpkg
+"""
+
+def write_ohq():
+    raise NotImplementedError("OHQ writer to be implemented.")
